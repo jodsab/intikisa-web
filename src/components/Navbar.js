4cd556bee469ccccd7d4964b469b087aa449cf32
jodsab <85377591+jodsab@users.noreply.github.com>
2021-10-29 19:28:42 -0500

    para sebas

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import navoptions from './navbar_menu.json';
+import './navbar.scss';
+
+function Navbar() {
+
+    return(
+        <div className='navbar_container'>
+            <div className='navbar_logo'>
+                <img src={require('../img/navbar/logointikisa.png').default} />
+                <div className='navbar_nav_menu'>
+                    <ul className='nav_list'>
+                        {
+                            navoptions.map((item) => (
+                                <li key={item.id}>
+                                    <p>{item.section}</p>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+            </div>
+            <div className='navbar_cuenta'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
