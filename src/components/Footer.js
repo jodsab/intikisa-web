59d00511d2b6ab5b3d53ce0ff282a1884b5b16a9
sebas1222 <55025376+sebas1222@users.noreply.github.com>
2021-10-30 23:26:45 -0500

    footerup2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,33 +2,32 @@ import { FaFacebook } from "react-icons/fa";
 import { SiInstagram } from "react-icons/si";
 import { FaTiktok } from "react-icons/fa"
 import './footer.scss';
+import footeroptions from './footer_container.json';
 
 function Footer() {
     return(
         <div className="footer_container">
-         <div className="item1">
+         <div className="logo_footer">
          <img src={require('../img/navbar/logointikisa.png').default} />
          </div>
-         <div className='items_div'>
-            <div className="item2">
-                <h1>Tiendas y Servicios</h1>
-                <p>Zonas de reparto</p>
-            </div>
-            <div className="item3">
-                <h1>Atención al cliente</h1>
-                <p>Contáctanos</p>
-            </div>
-            <div className="item4">
-                <h1>Políticas y terminos</h1>
-                <p>Siguenos</p>
-            </div>
-         </div>
-         <div className="redes_sociales_container">
+         <div className='options_div'>
+         <ul className='options_list'>
+                    {
+                        footeroptions.map((item) => (
+                             <li key={item.id}>
+                                <h1>{item.section}</h1>
+                                <p>{item.sub_section}</p>
+                             </li>
+                        ))
+                        }
+                    </ul>
+        </div>
+        <div className="redes_sociales_container">
+            <p>Siguenos</p>
             <FaFacebook className="rs_icon"/>
             <SiInstagram className="rs_icon"/>
             <FaTiktok className="rs_icon"/>
          </div>       
-         
         </div>
         /*<div>
             <FaFacebook />
