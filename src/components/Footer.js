5ced961e6600946a4c7316082dd968163d833fdb
sebas1222 <55025376+sebas1222@users.noreply.github.com>
2021-10-31 22:45:06 -0600

    footerup32_10

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 import { FaFacebook } from "react-icons/fa";
 import { SiInstagram } from "react-icons/si";
-import { FaTiktok } from "react-icons/fa"
+import { FaTiktok } from "react-icons/fa";
+import { RiVisaFill } from "react-icons/ri";
+import { FaCcMastercard } from "react-icons/fa";
+import { FaCcPaypal} from "react-icons/fa";
 import './footer.scss';
 import footeroptions from './footer_container.json';
 
@@ -16,27 +19,34 @@ function Footer() {
                         footeroptions.map((item) => (
                              <li key={item.id}>
                                 <h1>{item.section}</h1>
-                                <h2>{item.half_border}</h2>
+                                <h2></h2>
                                 <p>{item.sub_section}</p>
                                 <p>{item.sub_section2}</p>
                                 <p>{item.sub_section3}</p>
                                 <p>{item.sub_section4}</p>
-                             </li>
+                             </li> 
                         ))
+                        
                         }
-                    </ul>
+                        <div className="redes_sociales_container">
+                        <FaFacebook className="rs_icon"/>
+                        <SiInstagram className="rs_icon"/>
+                        <FaTiktok className="rs_icon"/>
+                       </div> 
+         </ul>
+ 
         </div>
-        <div className="redes_sociales_container">
-            <p>Siguenos</p>
-            <FaFacebook className="rs_icon"/>
-            <SiInstagram className="rs_icon"/>
-            <FaTiktok className="rs_icon"/>
+        <div className="bottom_footer_container">
+        <div className="derechos_container">
             <p>Copyright © Intikisa Corp. Todos los derechos reservados 2021.</p>
-         </div>       
+         </div>
+         <div className="metodos_pago_container">
+         <RiVisaFill className="rs_icon" />
+         <FaCcMastercard className="rs_icon"/>
+         <FaCcPaypal className="rs_icon"/>
+             </div>  
+             </div>     
         </div>
-        /*<div>
-            <FaFacebook />
-        </div> */
     )
 }
 
