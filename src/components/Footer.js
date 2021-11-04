4d9cafbd68e03a00e3a8d16b3298e914b3670518
sebas1222 <55025376+sebas1222@users.noreply.github.com>
2021-11-04 09:35:29 -0600

    footerup4/11

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,8 +10,8 @@ import footeroptions from './footer_container.json';
 function Footer() {
     return(
         <div className="footer_container">
-         <div className="logo_footer">
-         <img src={require('../img/navbar/logointikisa.png').default} />
+         <div>
+         <img src={require('../img/navbar/logointikisa.png').default} className="logo_footer" />
          </div>
          <div className='options_div'>
          <ul className='options_list'>
@@ -19,6 +19,7 @@ function Footer() {
                         footeroptions.map((item) => (
                              <li key={item.id}>
                                 <h1>{item.section}</h1>
+                                <h2></h2>
                                 <p><a href="" target="_blank">{item.sub_section}</a></p>
                                 <p><a href="" target="_blank">{item.sub_section2}</a></p>
                                 <p><a href="" target="_blank">{item.sub_section3}</a></p>
@@ -30,6 +31,7 @@ function Footer() {
                         }      
                             <li>
                               <h1>Nuestras redes</h1>
+                              <h2></h2>
                               <div className="redes_sociales_container">
                                <a href="https://www.facebook.com" target="_blank" >
                                <FaFacebook className="rs_icon"/>
@@ -48,13 +50,13 @@ function Footer() {
          <div className="derechos_container">
             <span> © 2021 Intikisa Corp. </span> 
             <span>Todos los derechos reservados.</span>
-         </div>
-         <div className="metodos_pago">
+         </div> 
+        <div className="metodos_pago">
          <RiVisaFill className="rs_icon" />
          <FaCcPaypal className="rs_icon"/>
          <FaCcMastercard className="rs_icon"/>
          </div>
-        </div>  
+        </div>
         </div>   
     )
 }
